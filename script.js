fefb9be2e0df0db0e656f351f274b3042e38161b
AndreKing <andre.king81@gmail.com>
2021-11-11 16:18:00 -0500

    Loop Challenges

---
+++ script.js
@@ -0,0 +1,35 @@
+//Prints odds 1-20
+console.log("The odd numbers 1-20 are: ")
+for (let i = 1; i < 21; i++) {
+    if (i % 2 == 1) {
+    console.log(i)}
+  } 
+
+//Decreasing Multiples of 3
+console.log("The numbers less than 100 that are divisible by 3 are: ")
+for (let i = 100; i > 0; i--) {
+    if (i % 3 == 0) {
+    console.log(i)}
+  } 
+
+  //Decreasing Multiples of 3
+console.log("The printed numbers in the sequence are as follows: ")
+for(let i = 4; i >- 4; i -= 1.5) {
+    console.log(i);
+}
+
+//Sigma
+console.log("The answers for sigma are: ")
+sum = 0;
+for (let i = 1; i < 101; i++) {
+    sum = i + sum;
+    console.log(sum)    
+}  
+
+//Factorial
+console.log("The answers for factorial are: ")
+product = 1;
+for (let i = 1; i < 13; i++) {
+    product = i * product;
+    console.log(product)    
+}  
