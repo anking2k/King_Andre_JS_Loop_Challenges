5dbf2758f3dc787450ff94e603a78244b5dc0ad8
AndreKing <andre.king81@gmail.com>
2021-11-11 16:19:40 -0500

    Loop Challenges

--- script.js
+++ script.js
@@ -3,14 +3,14 @@ console.log("The odd numbers 1-20 are: ")
 for (let i = 1; i < 21; i++) {
     if (i % 2 == 1) {
     console.log(i)}
-  } 
+} 
 
 //Decreasing Multiples of 3
 console.log("The numbers less than 100 that are divisible by 3 are: ")
 for (let i = 100; i > 0; i--) {
     if (i % 3 == 0) {
     console.log(i)}
-  } 
+} 
 
   //Decreasing Multiples of 3
 console.log("The printed numbers in the sequence are as follows: ")
